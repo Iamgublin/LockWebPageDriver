6e1fa8454c2cf623ec6cf7418d74e5165a3ece5e
Iamgublin <403760189@qq.com>
2017-08-31 00:36:34 +0800

    2017/8/31

--- lock/MyDriver3/main.c
+++ lock/MyDriver3/main.c
@@ -2,21 +2,37 @@
 #include<ntddk.h>
 #pragma warning(disable:4100)
 #pragma warning(disable:4152)
+typedef struct _GLOBAL
+{
+	PDEVICE_OBJECT Dev;
+	PKEVENT UserModeWaitEvent;
+	BOOLEAN WaitFor360Se;
+	BOOLEAN Is360SePathGet;
+	WCHAR T360sePath[255];
+}GLOBAL,*PGLOBAL;
+GLOBAL Global;
+
+
 #define DEVICE_NAME L"\\Device\\Contact Device"
+#define SYM_NAME L"\\??\\LockPageDriver"
+
 typedef PPEB(__fastcall *P_PsGetProcessPeb)(PEPROCESS);
 typedef CHAR*(__fastcall *F_QueryProcessImageFileName)(PEPROCESS);
 
 
 P_PsGetProcessPeb PsGetProcessPeb = NULL;
 F_QueryProcessImageFileName QueryProcessImageFileName = NULL;
-UNICODE_STRING Ie = RTL_CONSTANT_STRING(L"iexplore.exe");
+
+
+
 const WCHAR* LockUrl = L" https://hao.360.cn";
-PDEVICE_OBJECT Dev = NULL;
 
-#define URL L"C:\\Users\\zlzPC\\AppData\\Roaming\\360se6\\Application\\360se.exe http://www.baidu.com/"
+#define URL L"\"C:\\Users\\zlzPC\\AppData\\Roaming\\360se6\\Application\\360se.exe\"   \"open http://www.baidu.com/\""
+#define IOCTL_SENDEVENTTOKERNEL (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x911,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA) 
+#define IOCTL_WAITFOR360SE (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x912,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA)
+#define IOCTL_GET360SEPATH (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x913,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA) 
 
-
-void LockIe(PEPROCESS CurrentProcess)
+void LockIe(PEPROCESS CurrentProcess, PPS_CREATE_NOTIFY_INFO CreateInfo)
 {
 	PPEB iePeb = NULL;
 	iePeb = PsGetProcessPeb(CurrentProcess);
@@ -33,26 +49,43 @@ void LockIe(PEPROCESS CurrentProcess)
 	KeDetachProcess();
 }
 
-void Lock360Se(PEPROCESS CurrentProcess)
+void Lock360Se(PEPROCESS CurrentProcess, PPS_CREATE_NOTIFY_INFO CreateInfo)
 {
-	PPEB iePeb = NULL;
+	/*PPEB iePeb = NULL;
 	iePeb = PsGetProcessPeb(CurrentProcess);
 	KeAttachProcess(CurrentProcess);
 	if (iePeb != NULL)
 	{
 		ULONG_PTR* param = (ULONG_PTR*)*((ULONG_PTR*)((ULONG_PTR)iePeb + 0x20));
-		PUNICODE_STRING commandline = (PUNICODE_STRING)((ULONG_PTR)param + 0x70);
-		commandline->MaximumLength += 100;
+		PUNICODE_STRING commandline = (PUNICODE_STRING)((ULONG_PTR)param + 0x70);*/
+		/*commandline->MaximumLength += 100;*/
 		/*NTSTATUS Sta = RtlAppendUnicodeToString(commandline, LockUrl);*/
-		RtlZeroMemory(commandline->Buffer, commandline->MaximumLength);
-		wcscpy(commandline->Buffer, URL);
+		/*RtlZeroMemory(commandline->Buffer, commandline->MaximumLength);
+		wcscpy(commandline->Buffer, URL);*/
 		/*DbgPrint("sta:0x%x\n", Sta);*/
-		DbgPrint("command:%ws\n", commandline->Buffer);
+		/*DbgPrint("command:%ws\n", commandline->Buffer);
 	}
-	KeDetachProcess();
+	KeDetachProcess();*/
+	if (!Global.Is360SePathGet)
+	{
+		WCHAR* str = wcsstr(CreateInfo->CommandLine->Buffer, L".exe");
+		str += 4;
+		memcpy(Global.T360sePath, CreateInfo->CommandLine->Buffer,(str- CreateInfo->CommandLine->Buffer)* sizeof(WCHAR));
+		DbgPrint("360sePath:%ws\n", Global.T360sePath);
+		Global.Is360SePathGet = TRUE;
+	}
+	if (Global.WaitFor360Se)
+	{
+		if (Global.UserModeWaitEvent != NULL)
+		{
+			CreateInfo->CreationStatus = STATUS_UNSUCCESSFUL;
+			KeSetEvent(Global.UserModeWaitEvent, IO_NO_INCREMENT, FALSE);
+		}
+		Global.WaitFor360Se = FALSE;
+	}
 }
 
-void LockChrome(PEPROCESS CurrentProcess)
+void LockChrome(PEPROCESS CurrentProcess, PPS_CREATE_NOTIFY_INFO CreateInfo)
 {
 	PPEB iePeb = NULL;
 	iePeb = PsGetProcessPeb(CurrentProcess);
@@ -82,17 +115,17 @@ void SetCreateProcessNotifyRoutineEx(
 	if (CreateInfo)
 	{
 		DbgPrint("start: %s\n", ProcessName);
-		if (strcmp(ProcessName, "iexplore.exe") == 0)
+		if (strstr(ProcessName, "iexplore.exe")!=NULL)
 		{
-			LockIe(CurrentProcess);
+			LockIe(CurrentProcess,CreateInfo);
 		}
-		else if (strstr(ProcessName,"360se.exe"))
+		else if (strstr(ProcessName,"360se.exe")!=NULL)
 		{
-			Lock360Se(CurrentProcess);
+			Lock360Se(CurrentProcess,CreateInfo);
 		}
-		else if (strstr(ProcessName, "chrome.exe"))
+		else if (strstr(ProcessName, "chrome.exe")!=NULL)
 		{
-			LockChrome(CurrentProcess);
+			LockChrome(CurrentProcess, CreateInfo);
 		}
 	}
 	else
@@ -103,15 +136,86 @@ void SetCreateProcessNotifyRoutineEx(
 VOID Unload(PDRIVER_OBJECT Driver)
 {
 	PsSetCreateProcessNotifyRoutineEx(SetCreateProcessNotifyRoutineEx, TRUE);
+	if (Global.Dev != NULL)
+	{
+		IoDeleteDevice(Global.Dev);
+	}
 }
 
+NTSTATUS DevControl(PDEVICE_OBJECT Device, PIRP Irp)
+{
+	NTSTATUS Sta;
+	PIO_STACK_LOCATION sa = IoGetCurrentIrpStackLocation(Irp);
+	switch (sa->Parameters.DeviceIoControl.IoControlCode)
+	{
+	case IOCTL_SENDEVENTTOKERNEL:
+	{
+		HANDLE hUserEvent = *(HANDLE*)Irp->AssociatedIrp.SystemBuffer;
+		//如果从UserBuffer中获取，应设置为UserMode，否则设置为KernelMode
+		Sta=ObReferenceObjectByHandle(hUserEvent, EVENT_MODIFY_STATE, *ExEventObjectType, KernelMode, &Global.UserModeWaitEvent, NULL);
+		if (!NT_SUCCESS(Sta))
+		{
+			Irp->IoStatus.Status = STATUS_UNSUCCESSFUL;
+			IoCompleteRequest(Irp, IO_NO_INCREMENT);
+			return Irp->IoStatus.Status;
+		}
+		ObDereferenceObject(Global.UserModeWaitEvent);
+		/*KeSetEvent(UserModeWaitEvent, IO_NO_INCREMENT, FALSE);*/
+		Irp->IoStatus.Status = STATUS_SUCCESS;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+		return Irp->IoStatus.Status;
+	}
+	case IOCTL_WAITFOR360SE:
+	{
+		if (!Global.WaitFor360Se)
+		{
+			Global.WaitFor360Se = TRUE;
+		}
+		Irp->IoStatus.Status = STATUS_SUCCESS;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+		return Irp->IoStatus.Status;
+	}
+	case IOCTL_GET360SEPATH:
+	{
+		//去掉第一个"符号
+		memcpy(Irp->AssociatedIrp.SystemBuffer, Global.T360sePath+1, sizeof(Global.T360sePath)-1);
+		Irp->IoStatus.Information = sizeof(Global.T360sePath);
+		Irp->IoStatus.Status = STATUS_SUCCESS;
+		IoCompleteRequest(Irp, IO_NO_INCREMENT);
+		return Irp->IoStatus.Status;
+	}
+	default:
+		break;
+	}
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS DevCreate(PDEVICE_OBJECT Device, PIRP Irp)
+{
+	return STATUS_SUCCESS;
+}
+NTSTATUS DevClose(PDEVICE_OBJECT Device, PIRP Irp)
+{
+	return STATUS_SUCCESS;
+}
 NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING str)
 {
 	UNICODE_STRING unstrFunName;
 	UNICODE_STRING DevName;
+	UNICODE_STRING SymName;
 	RtlInitUnicodeString(&DevName, DEVICE_NAME);
+	RtlInitUnicodeString(&SymName, SYM_NAME);
+
+	Global.Dev = NULL;
+	Global.UserModeWaitEvent = NULL;
+	Global.WaitFor360Se = FALSE;
+	Global.Is360SePathGet = FALSE;
+
 	DbgBreakPoint();
 	driver->DriverUnload = Unload;
+	driver->MajorFunction[IRP_MJ_CREATE] = DevCreate;
+	driver->MajorFunction[IRP_MJ_CLOSE] = DevClose;
+	driver->MajorFunction[IRP_MJ_DEVICE_CONTROL] = DevControl;
 
 	RtlInitUnicodeString(&unstrFunName, L"PsGetProcessPeb");
 	PsGetProcessPeb = MmGetSystemRoutineAddress(&unstrFunName);
@@ -127,7 +231,7 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING str)
 		return STATUS_UNSUCCESSFUL;
 	}
 
-
+	
 	PsSetCreateProcessNotifyRoutineEx(SetCreateProcessNotifyRoutineEx, FALSE);
 
 	IoCreateDevice(driver,
@@ -135,10 +239,10 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver, PUNICODE_STRING str)
 		&DevName,
 		FILE_DEVICE_UNKNOWN,
 		FILE_DEVICE_SECURE_OPEN, FALSE,
-		&Dev);
+		&Global.Dev);
 
-	Dev->Flags = DO_BUFFERED_IO;
-	Dev->Flags &= ~DO_DEVICE_INITIALIZING;
-
+	Global.Dev->Flags = DO_BUFFERED_IO;
+	Global.Dev->Flags &= ~DO_DEVICE_INITIALIZING;
+	IoCreateSymbolicLink(&SymName, &DevName);
 	return STATUS_SUCCESS;
 }
--- lock/exe/exe.cpp
+++ lock/exe/exe.cpp
@@ -3,10 +3,60 @@
 
 #include "stdafx.h"
 #include<Windows.h>
-
+#define DEVICE_NAME L"\\Device\\Contact Device"
+#define SYM_NAME L"\\??\\LockPageDriver"
+#define IOCTL_SENDEVENTTOKERNEL (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x911,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA) 
+#define IOCTL_WAITFOR360SE (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x912,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA)
+#define IOCTL_GET360SEPATH (ULONG)CTL_CODE(FILE_DEVICE_UNKNOWN,0x913,METHOD_BUFFERED,FILE_WRITE_DATA|FILE_READ_DATA) 
+#define IE L"C:\\Program Files\\Internet Explorer\\iexplore.exe"  
+#define CMD L"open http://www.baidu.com/"  
 int main()
 {
-	CreateEvent(NULL, FALSE, FALSE, L"ZLZ");
+	STARTUPINFO sa = { 0 };
+	PROCESS_INFORMATION pi = { 0 };
+	DWORD Byteret = 0;
+	WCHAR T360SePath[255] = { 0 };
+
+	HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, NULL);
+
+	printf("0x%x\n", GetLastError());
+
+	HANDLE hDev = CreateFile(SYM_NAME, GENERIC_ALL, 0, NULL, OPEN_ALWAYS, NULL, NULL);
+
+	DeviceIoControl(hDev, IOCTL_SENDEVENTTOKERNEL, &hEvent, sizeof(hEvent), NULL, 0, &Byteret, NULL);
+	DeviceIoControl(hDev, IOCTL_WAITFOR360SE, NULL, 0, NULL, 0, &Byteret, NULL);
+
+	WaitForSingleObject(hEvent, INFINITE);
+
+	DeviceIoControl(hDev, IOCTL_GET360SEPATH, NULL, 0, T360SePath, sizeof(T360SePath), &Byteret, NULL);
+
+	wprintf(TEXT("get 360path:%ws\n"), T360SePath);
+
+	Sleep(3000);
+
+	CreateProcess(T360SePath,
+		TEXT("open http://www.baidu.com/"),
+		NULL,
+		NULL,
+		FALSE,
+		NULL,
+		NULL,
+		NULL,
+		&sa,
+		&pi);
+	printf("0x%x\n", GetLastError());
+
+	WCHAR *A = TEXT("C:\\Users\\zlzPC\\AppData\\Roaming\\360se6\\Application\\360se.exe");
+	CreateProcess(A,
+		TEXT("open http://www.baidu.com/"),
+		NULL,
+		NULL,
+		FALSE,
+		NULL,
+		NULL,
+		NULL,
+		&sa,
+		&pi);
 	Sleep(INFINITE);
     return 0;
 }
